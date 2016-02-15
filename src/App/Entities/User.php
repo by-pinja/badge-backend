c88d1e4522daf80381f8b6766f58cbb322edba81
tarlepp <tarmo.leppanen@kase.fi>
2016-02-15 20:51:26 +0200

    Fixed user entity to implement UserInterface so that application knows that this entity is a real user.

--- src/App/Entities/User.php
+++ src/App/Entities/User.php
@@ -12,6 +12,10 @@ use JsonSerializable;
 // Doctrine components
 use Doctrine\ORM\Mapping as ORM;
 
+// Symfony components
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
 // 3rd party components
 use Swagger\Annotations as SWG;
 
@@ -38,11 +42,21 @@ use Swagger\Annotations as SWG;
  *  )
  *
  * @ORM\Table(
- *      name="user"
+ *      name="user",
+ *      uniqueConstraints={
+ *          @ORM\UniqueConstraint(
+ *              name="uq_username",
+ *              columns={"username"}
+ *          ),
+ *          @ORM\UniqueConstraint(
+ *              name="uq_email",
+ *              columns={"email"}
+ *          ),
+ *      },
  *  )
  * @ORM\Entity
  */
-class User implements JsonSerializable
+class User implements AdvancedUserInterface, JsonSerializable
 {
     /**
      * User id
@@ -188,11 +202,33 @@ class User implements JsonSerializable
     }
 
     /**
-     * @return string
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return Role[] The user roles
      */
     public function getRoles()
     {
-        return $this->roles;
+        if (!is_array($this->roles)) {
+            $this->roles = explode(',', $this->roles);
+        }
+
+        $roles = $this->roles;
+
+        // Every user must have at least one role, per Silex security docs.
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     /**
@@ -205,6 +241,31 @@ class User implements JsonSerializable
     public function getIdentifier()
     {
         return $this->username;
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
     }
 
     /**
@@ -308,5 +369,80 @@ class User implements JsonSerializable
             'email'     => $this->getEmail(),
             'roles'     => $this->getRoles(),
         ];
+    }
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired()
+    {
+        // TODO: Implement isAccountNonExpired() method.
+        return true;
+    }
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked()
+    {
+        // TODO: Implement isAccountNonLocked() method.
+        return true;
+    }
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired()
+    {
+        // TODO: Implement isCredentialsNonExpired() method.
+        return true;
+    }
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled()
+    {
+        // TODO: Implement isEnabled() method.
+        return true;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
